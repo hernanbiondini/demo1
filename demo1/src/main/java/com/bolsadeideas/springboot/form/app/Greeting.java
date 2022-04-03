deea13c13e8248dafe1baf9d4e6f0bfba776a8bd
hernanbiondini <hernanbiondini@gmail.com>
2022-04-03 18:34:28 -0300

    Commit all files

---
+++ demo1/src/main/java/com/bolsadeideas/springboot/form/app/Greeting.java
@@ -0,0 +1,31 @@
+package com.bolsadeideas.springboot.form.app;
+
+public class Greeting {
+	
+private Long id;
+private String message;
+
+public Long getId() {
+	return id;
+}
+public void setId(Long id) {
+	this.id = id;
+}
+public String getMessage() {
+	return message;
+}
+public void setMessage(String message) {
+	this.message = message;
+}
+public Greeting(Long id, String message) {
+	super();
+	this.id = id;
+	this.message = message;
+}
+public Greeting(long incrementAndGet, String format) {
+	super();
+	this.id = incrementAndGet;
+	this.message = format;
+}
+
+}
